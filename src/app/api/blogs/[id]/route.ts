d53d2f140b586ff9bca38c9d36444c0ab5519521
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2025-07-24 10:43:01 +0700

    change request /blog/id

--- src/app/api/blogs/[id]/route.ts
+++ src/app/api/blogs/[id]/route.ts
@@ -1,7 +1,7 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { apiBlog } from "@/utils/api/blog.api";
 
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+export async function GET(req: Request, { params }: { params: { id: string } }) {
     try {
         const { id } = await params
         const response = await apiBlog.get(`/blogs/${id}`)
