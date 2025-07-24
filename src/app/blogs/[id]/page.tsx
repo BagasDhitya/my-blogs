b281c2b90a180de4d2c4bee10e4935ec6b7b58b0
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2025-07-24 11:21:05 +0700

    remove props

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,27 +1,24 @@
 import axios from "axios";
 
-export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic"; // penting agar route dinamis
 
-interface BlogDetailPageProps {
+export default async function BlogDetail({
+    params,
+}: {
     params: { id: string };
-}
-export default async function BlogDetail({ params }: BlogDetailPageProps) {
-    const { id } = params
-    const blog = await axios.get(`http://172.16.3.116:3000/api/blogs/${id}`)
-    // fetching api menggunakan teknik SSR (Server Side Rendering)
-    // dimana data dari api dirender terlebih dahulu oleh server, sebelum disajikan ke component
+}) {
+    const { id } = params;
 
-    const { title, author, image, content } = blog?.data?.data || {}
+    const blog = await axios.get(`http://172.16.3.116:3000/api/blogs/${id}`);
+
+    const { title, author, image, content } = blog?.data?.data || {};
 
     return (
         <main className="max-w-2xl mx-auto p-4">
             <h1 className="text-3xl font-bold mb-2">{title}</h1>
             <p className="text-gray-500 mb-4">{author}</p>
-            <img
-                className="w-full mb-4 rounded"
-                src={image}
-                alt={title} />
+            <img className="w-full mb-4 rounded" src={image} alt={title} />
             <p>{content}</p>
         </main>
-    )
+    );
 }
